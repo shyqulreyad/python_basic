a54549c39be2eb145aae63ea396572818422f1d6
shyqulreyad <shyqulislam@gmail.com>
2023-01-01 15:45:09 +0600

    keywords in python

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import keyword
 my_name = 'shyqulreyad'
 print(my_name)
-a, b, c = 10, 'hi', 1
+a, b, c = 10, True, 1
 print(b)
 print(a)
 print(c)
 res = a + c
 print (res)
 print(id(res))
+print(keyword.kwlist)
