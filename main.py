e3e74eb7acedf412461c4a27f631a105cde969dd
shyqulreyad <shyqulislam@gmail.com>
2023-01-01 15:57:52 +0600

    strings in python

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import keyword
 my_name = 'shyqulreyad'
-print(my_name)
+#strings with single quote in it should be define with "" quote
+title = "it's nice to learn python"
+print(title)
+#strings with multiple line should be define with tripple quote
+heading = '''it's a multiple line 
+heading
+like i can add more line into this'''
+print(heading)
+print('hi',my_name)
 a, b, c = 10, True, 1
 print(b)
 print(a)
